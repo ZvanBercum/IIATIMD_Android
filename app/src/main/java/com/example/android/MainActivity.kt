9eee9d1d61fbf72aac9511cd0d3762cfbc45d2f7
Zissely van Bercum <zisvanbercum@hotmail.com>
2020-08-26 19:32:03 +0200

    login update

---
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.Resources.SharedPrefs
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Loas class for memory
+        val sharedPrefs = SharedPrefs(this)
+
+        //Check if user is logged in
+        val logged = sharedPrefs.getLogged()
+        if(logged){
+            this.switchToHome()
+        }else{
+            this.switchToLogin()
+        }
+    }
+
+    /**
+     * @func switchToLogin
+     * Load the login screen
+     */
+    fun switchToLogin(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    /**
+     * @func switchToHome
+     * Load the home screen if user is logged
+     */
+    fun switchToHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+
+}
