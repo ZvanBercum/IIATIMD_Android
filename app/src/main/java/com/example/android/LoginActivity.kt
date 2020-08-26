a75f6b14151d6d3c744e7770d694e946f26f7c07
Zissely van Bercum <zisvanbercum@hotmail.com>
2020-08-26 15:46:28 +0200

    Start of login

---
+++ app/src/main/java/com/example/android/LoginActivity.kt
@@ -0,0 +1,96 @@
+package com.example.android
+
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ipmedth.Resources.Api
+import com.example.ipmedth.Resources.JsonMaker
+
+
+class LoginActivity : AppCompatActivity() {
+
+    //Init view
+    lateinit var submit: Button
+    var result : String = ""
+
+    //Load classes
+    private val api = Api()
+    private val jsonMaker = JsonMaker()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        //Set variables
+        val loginNameEt = findViewById<EditText>(R.id.login_name)
+        val passwordEt = findViewById<EditText>(R.id.login_password)
+        submit = findViewById(R.id.sign_in_button)
+
+        //Submit login
+        submit.setOnClickListener{
+            val loginName = loginNameEt.text.toString()
+            val password = passwordEt.text.toString()
+            login(loginName, password)
+        }
+    }
+
+    /**
+     * @func login
+     * @var name String
+     * @var password String
+     * This function makes an api request to the server and handles the server response
+     * Then it makes the advice and saves the user
+     */
+    private fun login(name: String, password: String){
+        api.login(name, password) { result ->
+            if(result.toString() == "This user does not exist"){
+                makeErrorDialog("Inloggen mislukt!", "Deze gebruiker bestaat niet")
+            }else if(result.toString() == "\"This password doest not exist by this user\"") {
+                makeErrorDialog("Inloggen mislukt!", "Verkeerd wachtwoord opgegeven")
+            }else{
+                switchToHome()
+            }
+        }
+    }
+
+    /**
+     * @func switchToHome
+     * This function switches to home
+     */
+    private fun switchToHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    /**
+     * @func makeErrorDialog
+     * @var title String
+     * @var message String
+     * This function makes an error dialog to display
+     * errors that happen while logging in
+     */
+    private fun makeErrorDialog(title: String, message: String){
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("ok", DialogInterface.OnClickListener {
+                    dialog, id ->(dialog.dismiss())
+            })
+        val alert = dialogBuilder.create()
+        alert.show()
+    }
+
+    /**
+     * OVERIDE
+     * @func onBackPressed
+     * This function helps the user not return to being logged in
+     * if they logged out.
+     */
+    override fun onBackPressed(){
+        finishAffinity()
+    }
+}
