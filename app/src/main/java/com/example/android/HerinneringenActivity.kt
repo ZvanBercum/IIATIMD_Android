5a686b557016b76b212bfb9e618b1df7d22c14ac
Lars de Klijne <larsdeklijne@gmail.com>
2020-08-27 20:46:17 +0200

    push notificaties ingesteld

--- app/src/main/java/com/example/android/HerinneringenActivity.kt
+++ app/src/main/java/com/example/android/HerinneringenActivity.kt
@@ -1,18 +1,66 @@
 package com.example.android
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.example.android.Resources.Api
-import com.example.android.Resources.SharedPrefs
+import kotlinx.android.synthetic.main.activity_herrineringen.*
 
 class HerinneringenActivity : AppCompatActivity()  {
 
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+    private val channelId = "com.example.android"
+    private val description = "Medicine reminder notification"
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("DEBUG", "GEKLIIKT!!!!!!!!!!!!!!!!!")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_herrineringen)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notification.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, FLAG_UPDATE_CURRENT)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                notificationChannel = NotificationChannel(channelId,description,NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(false)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this, channelId)
+                    .setContentTitle("Medicine reminder")
+                    .setContentText("Test notificatie")
+                    .setSmallIcon(R.drawable.ic_launcher_round)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher))
+                    .setContentIntent(pendingIntent)
+            } else {
+                builder = Notification.Builder(this, channelId)
+                    .setContentTitle("Medicine reminder")
+                    .setContentText("Test notificatie")
+                    .setSmallIcon(R.drawable.ic_launcher_round)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher))
+                    .setContentIntent(pendingIntent)
+            }
+            notificationManager.notify(1234,builder.build())
+        }
+
     }
 
 }
